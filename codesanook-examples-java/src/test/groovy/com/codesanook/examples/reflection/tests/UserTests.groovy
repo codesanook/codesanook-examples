32e0495ecda66651e846cd0f9f4f54327aac570e
aaronamm <theeranitp@gmail.com>
2018-01-31 22:16:32 +0700

    unit tests for reflection in Java

--- codesanook-examples-java/src/test/groovy/com/codesanook/examples/reflection/tests/UserTests.groovy
+++ codesanook-examples-java/src/test/groovy/com/codesanook/examples/reflection/tests/UserTests.groovy
@@ -2,6 +2,7 @@ package com.codesanook.examples.reflection.tests
 
 import com.codesanook.examples.reflection.User
 import spock.lang.Specification
+import java.lang.reflect.*;
 
 class UserTests extends Specification {
     def "getFullName should return correct full name"() {
@@ -13,5 +14,28 @@ class UserTests extends Specification {
             def result = user.getFullName()
         then:
             result == "AA BB"
+    }
+
+    def "getDeclaredMethods with Reflection should return 5 method"() {
+        given:
+            def user = new User()
+        when:
+            Class userClass = user.getClass();
+            Method[] methods = userClass.getDeclaredMethods();
+        then:
+            methods.length == 5
+    }
+
+    def "invoke getFullName method with Reflection should return correct full name"() {
+        given:
+            def user = new User()
+            user.setFirstName("AA")
+            user.setLastName("BB")
+        when:
+            Class userClass = user.getClass()
+            Method method = userClass.getMethod("getFullName")
+            def fullName = method.invoke(user, null)
+        then:
+            fullName == "AA BB"
     }
 }
