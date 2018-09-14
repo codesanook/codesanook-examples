dc7324884ee726c04fa122f17b99428fd980ecc1
aaronamm <theeranitp@gmail.com>
2018-09-14 07:40:05 +0700

    - add AWS.Logger.Log4net - setup log4net console appender - use UTC ISO8601 format for date layout

---
+++ CodeSanook.Examples.CloudWatchLog/Program.cs
@@ -0,0 +1,21 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSanook.Examples.CloudWatchLog
+{
+    public class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
+        public static void Main(string[] args)
+        {
+            log.Debug("Hello world logging.");
+            log.Info("Hello world logging.");
+            log.Error("Hello world logging.");
+        }
+    }
+}
