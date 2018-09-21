1628bc2c960f71a1578112b98502cb82dd368f1a
aaronamm <theeranitp@gmail.com>
2018-09-21 18:46:30 +0700

    Add example of async exception

---
+++ CodeSanook.Examples.Async/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSanook.Examples.Async
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var task1 = Task.Run(() =>
+            {
+                throw new Exception("handle exception in task1");
+            });
+
+            var task2 = Task.Run(() =>
+            {
+                throw new Exception("handle exception in task2");
+            });
+
+            var task3 = Task.Run(() =>
+            {
+                Task.Factory.StartNew(() => throw new Exception("inner unhandle exception inside 3"), TaskCreationOptions.AttachedToParent);
+                throw new Exception("unhandle exception in task3");
+            });
+
+            try
+            {
+              Task.WaitAll(task3, task1, task2);
+            }
+            catch (Exception ex)
+            {
+                var inner = ex.InnerException;
+                var inners = ((AggregateException)ex).Flatten().InnerExceptions;
+
+            }
+        }
+    }
+}
