869cb15b3c55e6827610b26db7f82528850eecc6
aaronamm <theeranitp@gmail.com>
2018-09-24 23:35:34 +0700

    - register listener for onHistoryStateUpdated - IIFE to prevent to scoped variable

--- codesanook-examples-chrome-extension/src/extension/background.ts
+++ codesanook-examples-chrome-extension/src/extension/background.ts
@@ -1,29 +1,53 @@
 declare let chrome: any;
+const boardNameRule = "boardNameRule";
 
-chrome.runtime.onInstalled.addListener(() => {
-    console.log("setting up for working with content script");
-    //https://developer.chrome.com/apps/messaging
-    chrome.pageAction.onClicked.addListener(() => {
-        chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-            chrome.tabs.sendMessage(tabs[0].id, {}, response => {
-
+//Self-Invoking Functions
+//http://peter.michaux.ca/articles/an-important-pair-of-parens
+//http://benalman.com/news/2010/11/immediately-invoked-function-expression/
+//https://www.w3schools.com/js/js_function_definition.asp
+//http://adripofjavascript.com/blog/drips/an-introduction-to-iffes-immediately-invoked-function-expressions.html
+//https://codeburst.io/javascript-what-the-heck-is-an-immediately-invoked-function-expression-a0ed32b66c18
+(function () {
+    chrome.runtime.onInstalled.addListener(() => {
+        console.log("setting up for listener to communicate with a content script");
+        //https://developer.chrome.com/apps/messaging
+        chrome.pageAction.onClicked.addListener(() => {
+            chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+                chrome.tabs.sendMessage(tabs[0].id, {}, response => {
+                    console.log("got response from content script");
+                });
+                console.log("message sent to content script");
             });
-            console.log("message sent");
+        });
+
+        chrome.declarativeContent.onPageChanged.removeRules([boardNameRule], () => {
+            chrome.declarativeContent.onPageChanged.addRules([{
+                id: boardNameRule,
+                conditions: [
+                    new chrome.declarativeContent.PageStateMatcher({
+                        pageUrl: {
+                            //https://github.com/google/re2/blob/master/doc/syntax.txt
+                            urlMatches: "(?i)dev-current-development" //case insensitive
+                        }
+                    })
+                ],
+                actions: [new chrome.declarativeContent.ShowPageAction()]
+            }]);
         });
     });
 
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {
-                    //https://github.com/google/re2/blob/master/doc/syntax.txt
-                    urlMatches: "(?i)dev-current-development" //case insensitive
-                },
-            })],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-    });
-});
+    //https://stackoverflow.com/a/21071357/1872200
+    let listener = details => {
+        console.log("on onHistoryStateUpdated");
+        chrome.tabs.executeScript(null, { file: "content.js" });
+    }
 
+    if (chrome.webNavigation.onHistoryStateUpdated.hasListener(listener)) {
+        console.log("removed onHistoryStateUpdated listener");
+        chrome.webNavigation.onHistoryStateUpdated.addListener(listener);
+    }
 
-console.log("background script loaded");
+    chrome.webNavigation.onHistoryStateUpdated.removeListener(listener);
+    console.log("added onHistoryStateUpdated listener");
+    console.log("background loaded");
+})();
--- codesanook-examples-chrome-extension/src/extension/content.ts
+++ codesanook-examples-chrome-extension/src/extension/content.ts
@@ -1,20 +1,31 @@
 import { CardList } from "../models/CardList";
 declare let chrome: any;
-//run at
-//https://developer.chrome.com/extensions/content_scripts#run_time
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    //update point to each list
-    let cardLists = CardList.createCardLists();
-    cardLists.forEach(list => list.setPoint(".list-point", list => `&Sigma; (${list.point})`));
 
-    //update total points
-    let sprintList = CardList.getSprintList(cardLists);
-    let totalPoints = CardList.getTotalPoint(cardLists);
+(function () {
+    //run at
+    //https://developer.chrome.com/extensions/content_scripts#run_time
+    let onMessageListener = (request, sender, sendResponse) => {
+        //update point to each list
+        let cardLists = CardList.createCardLists();
+        cardLists.forEach(list => list.setPoint(".list-point", list => `&Sigma; (${list.point})`));
+
+        //update total points
+        let sprintList = CardList.getSprintList(cardLists);
+        let totalPoints = CardList.getTotalPoint(cardLists);
         sprintList.setPoint(
-        ".total-points",
-        (_) => `&Sigma; (${cardLists.map(list => list.point).join(" + ")} = ${totalPoints})`
-    );
-    console.log("point updated");
-});
+            ".total-points",
+            (_) => `&Sigma; (${cardLists.map(list => list.point).join(" + ")} = ${totalPoints})`
+        );
+        console.log("point updated");
+    };
 
-console.log("content script loaded");
+    if(chrome.runtime.onMessage.hasListener(onMessageListener)){
+        chrome.runtime.onMessage.removeListener(onMessageListener);
+        console.log("remove on message listener");
+    }
+
+    chrome.runtime.onMessage.addListener(onMessageListener);
+    console.log("add on message listener");
+
+    console.log("content script loaded");
+})();
