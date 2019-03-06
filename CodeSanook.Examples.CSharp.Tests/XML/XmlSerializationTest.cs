57330d7bc341769e857561b8c0386300cb1e27db
aaronamm <theeranitp@gmail.com>
2019-03-06 18:30:44 +0700

    Read XML with PowerShell XPath

---
+++ CodeSanook.Examples.CSharp.Tests/XML/XmlSerializationTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using Xunit;
+
+namespace CodeSanook.Examples.CSharp.Tests.XML
+{
+    [Serializable]
+    public class MyClass : Exception, ISerializable
+    {
+        public MyClass() { }
+        public MyClass(string message, Exception innerException) : base(message, innerException) { }
+
+        void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+            info.AddValue("message2", Message);
+        }
+    }
+
+    public class XmlSerializationTest
+    {
+
+        [Fact]
+        public void Test()
+        {
+            var xsSubmit = new XmlSerializer(typeof(MyClass));
+            var subReq = new MyClass();
+
+            using (var sww = new StringWriter())
+            {
+                using (var writer = XmlWriter.Create(sww))
+                {
+                    xsSubmit.Serialize(writer, subReq);
+                    Console.WriteLine(sww.ToString()); // Your XML
+                }
+            }
+        }
+    }
+}
--- CodeSanook.Examples.PowerShell/Tests/EnvironmentValueMatchTest.ps1
+++ CodeSanook.Examples.PowerShell/Tests/EnvironmentValueMatchTest.ps1
@@ -1,27 +1,85 @@
 
-$liveEndpoints = @(
-    @{ a = 1; b = 2; expectedResult = 3 }
-    @{ a = 5; b = 8; expectedResult = 13 }
-)
 
-$uatEndpoints = @(
-    @{  a = 1; b = 2; expectedResult = 3 }
-    @{ a = 5; b = 8; expectedResult = 13 }
-)
 
+function Get-ActualValue {
+
+    $Xml = [xml]@"
+<?xml version="1.0" encoding="utf-8"?>
+<Book>
+  <projects>
+    <project name="Book1" date="2009-01-20">
+      <editions>
+        <edition name="a" language="English">En.Book1.com</edition>
+        <edition name="b" language="German">Ge.Book1.Com</edition>
+        <edition name="c" language="French">Fr.Book1.com</edition>
+        <edition name="d" language="Polish">Pl.Book1.com</edition>
+      </editions>
+    </project>
+  </projects>
+</Book>
+"@
+
+
+    Select-Xml -xml $Xml -XPath "//edition[@name='a']/@language" | ForEach-Object { $_.node.Value }
+    Select-Xml -xml $Xml -XPath "//edition[@name='b']" | ForEach-Object { $_.node.InnerText }
+
+    return ""
+}
 
 Describe -Tag "LIVE" -Name "Pester basic command" -Fixture {
-    It "LIVE Adds <a> and <b> should return <expectedResult>" -TestCases $liveEndpoints {
-        param ( $a, $b, $expectedResult)
-        $a + $b | Should Be $expectedResult
+    Context "Web and API" {
+        $endpoints = @(
+            @{ configNode = "connectionString"; expectedLive = "live"; expectedTest = "test"  }
+            @{ configNode = "connectionString"; expectedLive = "live"; expectedTest = "test" }
+        )
+
+        It "Validate Web configuration" -TestCases $endpoints {
+            param ($configNode, $expectedLive, $expectedTest)
+            $environmentName = "live"
+
+            $expectedValue, $actutalValue = @($PSBoundParameters["expected$environmentName"], (Get-ActualValue))
+
+
+            #$parameterList = (Get-Command -Name $CommandName).Parameters;
+            #$parameterList | ForEach-Object {
+            #    (Get-Variable -Name $_.Values.Name -ErrorAction SilentlyContinue)
+            #}
+
+            $expectedValue -eq $actutalValue | Should Be $true
+        }
+
+    }
+
+    Context "Web" {
+        $endpoints = @(
+            @{ configNode = "connectionString"; expectedLive = ""; expectedTest = ""  }
+            @{ configNode = "connectionString"; expectedLive = ""; expectedTest = "" }
+        )
+        It "Validate Web configuration" -TestCases $endpoints {
+            param ( $configNode, $expectedLive, $expectedTest)
+            $expectedLive -eq $expectedTest | Should Be $true
+        }
+    }
+
+    Context "API" {
+        $endpoints = @(
+            @{ configNode = "connectionString"; expectedLive = ""; expectedTest = ""  }
+            @{ configNode = "connectionString"; expectedLive = ""; expectedTest = "" }
+        )
+
+        It "Validate Web configuration" -TestCases $endpoints {
+            param ( $configNode, $expectedLive, $expectedTest)
+            $expectedLive -eq $expectedTest | Should Be $true
+        }
     }
 }
 
-Describe -Tag "UAT" -Name "Pester basic command" -Fixture {
-    It "UAT Adds <a> and <b> should return <expectedResult>" -TestCases $uatEndpoints {
-        param ( $a, $b, $expectedResult)
-        $a + $b | Should Be $expectedResult
-    }
-}
+function GetExepctedValueForEnvironment {
+    param(
+        [hashtable] $TestCaseData,
+        [string] $EnvironmentName
+    )
 
-#"appsetting",
+    $TestCaseData["expected$EnvironmentName"] #Case insensitive key
+}
+#Example of useage Invoke-Pester -Tag LIVE
