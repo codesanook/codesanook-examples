0751ecf4e5d8bd520e40cd08545968fa5f065a44
aaronamm <theeranitp@gmail.com>
2019-05-28 21:11:05 +0700

    Add example of React server rendering by using ReactJS.NET

---
+++ ReactServerSideRendering/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ReactServerSideRendering.MvcApplication" Language="C#" %>
---
+++ ReactServerSideRendering/Scripts/Sample.jsx
@@ -0,0 +1,10 @@
+﻿const Sample = (props) => {
+    return (
+        <div>
+            <h1>Hello world {props.name}</h1>
+            <p>
+                How to be {props.name}
+            </p>
+        </div>
+    );
+};
