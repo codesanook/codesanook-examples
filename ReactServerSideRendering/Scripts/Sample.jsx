2aeef7e54ebe0095a3f505bbc607af54c5e054db
aaronamm <theeranitp@gmail.com>
2019-05-31 21:10:25 +0700

    Add example of React server side rendering

--- ReactServerSideRendering/Scripts/Sample.jsx
+++ ReactServerSideRendering/Scripts/Sample.jsx
@@ -1,10 +1,16 @@
 ﻿const Sample = (props) => {
+
+    const handleButtonClick = () => {
+        alert($`Hello ${props.name}`);
+    };
+
     return (
         <div>
             <h1>Hello world {props.name}</h1>
             <p>
                 How to be {props.name}
             </p>
+            <button onClick={handleButtonClick}>Say hello </button>
         </div>
     );
 };
