afe083275a0e7a7671e095feb1d259dba7abcb21
aaronamm <theeranitp@gmail.com>
2019-06-02 17:41:43 +0700

    Add nodesass and webpack to Sass example

---
+++ codesanook-examples-sass/index.js
@@ -0,0 +1,1 @@
+require('./scss/center-element.scss')
---
+++ codesanook-examples-sass/webpack.config.js
@@ -0,0 +1,40 @@
+﻿const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+/*
+Useful webpack setup link
+Setup webpack with Bootstrap
+https://getbootstrap.com/docs/4.0/getting-started/webpack/
+*/
+module.exports = {
+    entry: "./index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+    },
+    resolve: {
+        extensions: [".js"]
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [{
+                loader: MiniCssExtractPlugin.loader
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS modules
+            }, {
+                loader: "sass-loader", // compiles Sass to CSS, using Node Sass by default
+            }],
+            exclude: /node_modules/
+        }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "style.css"
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 9000
+    }
+};
