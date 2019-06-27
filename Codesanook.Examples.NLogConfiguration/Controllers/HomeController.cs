4e31a7e716984588b687e8dd311c4f98def71f49
aaronamm <theeranitp@gmail.com>
2019-06-27 16:31:32 +0700

    Add Codesanook.Examples.NLogConfiguration project for NLog configuration example

---
+++ Codesanook.Examples.NLogConfiguration/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using NLog;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Codesanook.Examples.NLogConfiguration.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private static readonly ILogger logger = LogManager.GetCurrentClassLogger();
+
+        public async Task<ActionResult> Index()
+        {
+            logger.Info($"first logger thread id {Thread.CurrentThread.ManagedThreadId}");
+            await LongRunningProcess();
+            logger.Info($"second logger thread id {Thread.CurrentThread.ManagedThreadId}");
+            return View();
+        }
+
+        private Task LongRunningProcess()
+        {
+            var task = Task.Run(() =>
+             {
+                 Thread.Sleep(2000);
+                 var localLogger = LogManager.GetCurrentClassLogger();
+                 localLogger.Info($"local logger thread id {Thread.CurrentThread.ManagedThreadId}");
+             });
+            return task;
+        }
+    }
+}
---
+++ Codesanook.Examples.NLogConfiguration/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Codesanook.Examples.NLogConfiguration.MvcApplication" Language="C#" %>
