d173e4274c1de63486d63675e3eb2612e3967764
aaronamm <theeranitp@gmail.com>
2019-07-20 22:09:12 +0700

    Add DirSync job

---
+++ CodeSanook.Examples.PowerShell/Windows/New-DirSyncScheduledTask.ps1
@@ -0,0 +1,25 @@
+$command = {
+    try {
+        & "C:\ProgramData\chocolatey\lib\dirsyncpro\tools\DirSyncPro-1.53-Windows\DirSyncPro.exe" /analyze /nogui /quit "C:\tools\sync-job.dsc"
+        & "C:\ProgramData\chocolatey\lib\dirsyncpro\tools\DirSyncPro-1.53-Windows\DirSyncPro.exe" /sync /nogui /quit "C:\tools\sync-job.dsc"
+    }
+    catch {
+        $logPath = "c:\logs"
+        New-Item -Path $logPath -ItemType Directory -Force # New folder if not exist
+        $_.Exception | Out-File (Join-Path -Path $logPath -ChildPath 'syncing-error.txt')
+    }
+}
+
+$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-Command & { $command }" 
+
+$repeat = [timespan]::FromHours(2)
+$trigger = New-ScheduledTaskTrigger -At (Get-Date).AddMinutes(1) -Once -RepetitionInterval $repeat -RepetitionDuration ([timespan]::MaxValue) 
+$user = "NT AUTHORITY\SYSTEM" # Specify the account to run the script
+
+Register-ScheduledTask `
+    -TaskName "ShutdownTheInstance" `
+    -Trigger $trigger `
+    -User $user `
+    -Action $action `
+    -RunLevel Highest `
+    -Force
