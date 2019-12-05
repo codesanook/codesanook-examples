524c8b211d670cf66a9d6957c9676106b07b747a
aaronamm <theeranitp@gmail.com>
2019-12-05 08:43:13 +0700

    Add OpenGL with C# example

---
+++ Codesanook.Examples.OpenGL/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+
+namespace Codesanook.Examples.OpenGL
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var window = new GameWindow(500, 500);
+            var game =new Game(window);
+        }
+    }
+}
