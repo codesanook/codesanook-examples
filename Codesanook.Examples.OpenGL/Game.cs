7d50de1dd7fa5789a452eb5efb59305c7d339d49
aaronamm <theeranitp@gmail.com>
2019-12-07 09:47:49 +0700

    Draw triangle with setting up custom projection coordidate

---
+++ Codesanook.Examples.OpenGL/Game.cs
@@ -0,0 +1,51 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System;
+
+namespace Codesanook.Examples.OpenGL
+{
+    public class Game
+    {
+        private readonly GameWindow window;
+
+        public Game(GameWindow window)
+        {
+            this.window = window;
+            Start();
+        }
+
+        private void Start()
+        {
+            window.Load += Load;
+            window.Resize += Resize;
+            window.RenderFrame += RenderFrame;
+            window.Run(1.0 / 60.0);//60 frames in 1 second	
+        }
+
+        private void Resize(object sender, EventArgs e)
+        {
+            GL.Viewport(0, 0, window.Width, window.Height);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(0.0, 50.0, 0.0, 50.0, -1.0, 1.0);
+            GL.MatrixMode(MatrixMode.Modelview);
+        }
+
+        private void RenderFrame(object sender, FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Begin(BeginMode.Triangles);
+            GL.Vertex2(1.0, 1.0);
+            GL.Vertex2(49.0, 1.0);
+            GL.Vertex2(25.0, 49.0);
+            GL.End();
+            window.SwapBuffers();
+
+        }
+
+        private void Load(object sender, EventArgs e)
+        {
+            GL.ClearColor(1.0F, 0.0F, 0.0F, 0.0F);
+        }
+    }
+}
--- Codesanook.Examples.OpenGL/Program.cs
+++ Codesanook.Examples.OpenGL/Program.cs
@@ -1,19 +1,41 @@
-﻿using System;
+﻿using OpenTK;
+using System;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace Codesanook.Examples.OpenGL
 {
     public class Program
     {
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
+        //[STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            var handle = GetConsoleWindow();
+
+            // Hide
+            ShowWindow(handle, SW_HIDE);
+
+            // Show
+            //ShowWindow(handle, SW_SHOW);
+
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            var window = new GameWindow(500, 500);
+            //window.Run(1.0/60.0);//60 FPS
+            var game = new Game(window);
         }
 
         static void GameLoop(double elapsedTime)
