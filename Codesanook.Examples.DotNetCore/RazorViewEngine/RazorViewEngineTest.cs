4dc67e5fde3a0eeb1b06f233435e5a1493e9b495
aaronamm <theeranitp@gmail.com>
2020-07-11 23:24:09 +0700

    Add Razor View Engine example

---
+++ Codesanook.Examples.DotNetCore/RazorViewEngine/RazorViewEngineTest.cs
@@ -0,0 +1,28 @@
+﻿using Razor.Templating.Core;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Codesanook.Examples.DotNetCore.RazorViewEngine
+{
+    // This test requires Razor.Templates and set as Microsoft.NET.Sdk.Razor
+    // https://medium.com/@soundaranbu/render-razor-view-cshtml-to-string-in-net-core-7d125f32c79
+    public class RazorViewEngineTest
+    {
+        public RazorViewEngineTest()
+        {
+            RazorTemplateEngine.Initialize();
+        }
+
+        [Fact]
+        public async Task RenderAsync_ValidView_ReturnRenderedHtmlContent()
+        {
+            var viewData = new Dictionary<string, object>();
+            viewData["Value1"] = "1";
+            viewData["Value2"] = "2";
+            var html = await RazorTemplateEngine.RenderAsync("/Views/ExampleView.cshtml", new { }, viewData);
+            // Assert
+            Assert.NotNull(html);
+        }
+    }
+}
