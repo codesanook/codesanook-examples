29cd48075a6769aa8511be2ad2bbb8eb8aef4796
aaronamm <theeranitp@gmail.com>
2020-08-02 12:09:48 +0700

    Add Users table

--- codesanook-examples-sql/create-db.sql
+++ codesanook-examples-sql/create-db.sql
@@ -1,4 +1,4 @@
-Use master
+USE master
 GO
 
 SELECT @@VERSION
@@ -12,6 +12,26 @@ END
 ELSE
 BEGIN
     PRINT 'DB exists, not create a new one'
+END
+GO
+
+-- Switch to Codesanook database
+USE Codesanook
+GO
+
+-- U is a user-defined table
+-- https://docs.microsoft.com/en-us/sql/relational-databases/system-catalog-views/sys-objects-transact-sql?view=sql-server-ver15
+IF OBJECT_ID('Users', 'U') IS NULL
+BEGIN
+    CREATE TABLE [dbo].[Users]
+    (
+        [Id] [INT] NOT NULL IDENTITY(1,1),
+        [Email] [nvarchar](64) NOT NULL,
+        [FirstName] [nvarchar](64) NOT NULL,
+        [LastName] [nvarchar](64) NOT NULL,
+        [DateOfBirth] [datetime] NULL,
+        CONSTRAINT [PK_Users_Id] PRIMARY KEY CLUSTERED ([ID] ASC) -- Create a table with custom primary name
+    )
 END
 GO
 
