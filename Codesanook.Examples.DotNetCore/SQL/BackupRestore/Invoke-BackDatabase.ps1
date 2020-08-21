fb8b19f5745581986ba2cc35facb2b57e5999e5e
aaronamm <theeranitp@gmail.com>
2020-08-22 00:41:59 +0700

    Correct rename folder SQL => Sql

--- Codesanook.Examples.DotNetCore/SQL/BackupRestore/Invoke-BackDatabase.ps1
+++
@@ -1,3 +0,0 @@
-# Connect to master database to backup a database
-$connectionString = "Server=localhost, 1455; Database=master; User Id=SA; Password=12345Abc%"
-Invoke-Sqlcmd -ConnectionString $ConnectionString -InputFile "./backup-database.sql" | Format-Table -AutoSize
