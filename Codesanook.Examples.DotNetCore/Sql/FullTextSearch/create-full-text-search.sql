503a2ba84199f807c52a038ec4fd3981128a9e23
aaronamm <theeranitp@gmail.com>
2020-08-23 21:37:29 +0700

    Check if SQL object exist before creating

--- Codesanook.Examples.DotNetCore/Sql/FullTextSearch/create-full-text-search.sql
+++ Codesanook.Examples.DotNetCore/Sql/FullTextSearch/create-full-text-search.sql
@@ -2,43 +2,71 @@ USE AdventureWorks2012
 GO
 -- Full text search SQL example SQL Query
 
--- Create view with SCHEMABINDING
+-- Create view with SCHEMABINDING because we want to create an index to this view.
 -- What is SCHEMABINDING https://blog.sqlauthority.com/2019/10/06/what-is-schemabinding-in-sql-server-views-interview-question-of-the-week-245/
-CREATE view PersonPhoneNumbers 
-WITH SCHEMABINDING
-AS
-SELECT 
-    p.BusinessEntityID, 
-    f.PhoneNumber, 
-    p.FirstName 
-FROM 
-    Person.Person p
-    INNER JOIN Person.PersonPhone f
-    ON p.BusinessEntityID = f.BusinessEntityID
+
+IF OBJECT_ID('PersonPhoneNumbers', 'V') IS NULL
+BEGIN
+    EXEC ('
+        CREATE view PersonPhoneNumbers 
+        WITH SCHEMABINDING
+        AS
+
+        SELECT 
+            p.BusinessEntityID, 
+            f.PhoneNumber, 
+            p.FirstName 
+        FROM 
+            Person.Person p
+            INNER JOIN Person.PersonPhone f
+            ON p.BusinessEntityID = f.BusinessEntityID
+
+    ')
+END
 GO
 
--- Create a unique index
-CREATE UNIQUE CLUSTERED INDEX UX_PersonPhoneNumbers_BusinessEntityID
-ON dbo.PersonPhoneNumbers (BusinessEntityID)
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'UX_PersonPhoneNumbers_BusinessEntityID')
+BEGIN
+    -- Create a unique index
+    CREATE UNIQUE CLUSTERED INDEX UX_PersonPhoneNumbers_BusinessEntityID
+    ON dbo.PersonPhoneNumbers (BusinessEntityID)
+END
 GO
 
--- Create a full text catalogue
-CREATE FULLTEXT CATALOG AW2008FullTextDemoCatalog
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'IX_PersonPhoneNumbers_FullName')
+BEGIN
+    CREATE NONCLUSTERED INDEX [IX_PersonPhoneNumbers_FullName]
+    ON [dbo].[PersonPhoneNumbers] ([FirstName])
+    INCLUDE ([PhoneNumber])
+END
 GO
 
--- Create a full text index
-CREATE FULLTEXT INDEX ON dbo.PersonPhoneNumbers (
-    PhoneNumber,
-    FirstName
+IF NOT EXISTS (SELECT 1 FROM sys.fulltext_catalogs WHERE name = 'AW2008FullTextDemoCatalog')
+BEGIN
+    -- Create a full text catalogue
+    CREATE FULLTEXT CATALOG AW2008FullTextDemoCatalog
+END
+GO
+
+IF NOT EXISTS (
+    SELECT 1 
+    FROM sys.fulltext_indexes 
+    WHERE object_id = object_id('PersonPhoneNumbers') --table/view name 
 )
-KEY INDEX UX_PersonPhoneNumbers_BusinessEntityID 
-ON (AW2008FullTextDemoCatalog)
-WITH (CHANGE_TRACKING AUTO)
+BEGIN
+    -- Create a full text index
+    CREATE FULLTEXT INDEX ON dbo.PersonPhoneNumbers (
+        PhoneNumber,
+        FirstName
+    )
+    KEY INDEX UX_PersonPhoneNumbers_BusinessEntityID
+    ON (AW2008FullTextDemoCatalog)
+    WITH (CHANGE_TRACKING AUTO)
+END
 GO
 
 -- Optionally how to add column to full text index
 -- ALTER FULLTEXT INDEX ON dbo.PersonPhoneNumbers ADD (PhoneNumber)
-
 -- Query full text
 SELECT * 
 FROM dbo.PersonPhoneNumbers
@@ -58,4 +86,15 @@ GO
 SELECT * 
 FROM dbo.PersonPhoneNumbers
 WHERE FREETEXT ((PhoneNumber, FirstName), '949 ken')
+GO
+
+
+SELECT * 
+FROM dbo.PersonPhoneNumbers
+WHERE CONTAINS ( FirstName, '"ke*"')
+GO
+
+SELECT * 
+FROM dbo.PersonPhoneNumbers
+WHERE FirstName LIKE 'ke%'
 GO
