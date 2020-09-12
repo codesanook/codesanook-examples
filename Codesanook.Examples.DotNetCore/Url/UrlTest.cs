9597db64674fe770410d6e89b1004ee754bdf06f
aaronamm <theeranitp@gmail.com>
2020-09-12 16:46:29 +0700

    Add Flurl URL example

---
+++ Codesanook.Examples.DotNetCore/Url/UrlTest.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+using UrlHelper = Flurl.Url;
+
+namespace Codesanook.Examples.DotNetCore.Url
+{
+    public class UrlTest
+    {
+        [Fact]
+        public void AppendPathSegments_ValidMultiplePath_ReturnCorrectUrl()
+        {
+            var uriBuildler = new UriBuilder()
+            {
+                Scheme = "https",
+                Host = "https://www.abc.com"
+            };
+
+            var path1 = "xx";
+            var path2 = "yy";
+
+            // https://www.hanselman.com/blog/UsingFlurlToEasilyBuildURLsAndMakeTestableHttpClientCallsInNET.aspx
+            var url = new UrlHelper(uriBuildler.Uri);
+            url.AppendPathSegments(path1, path2);
+
+            Assert.Equal("https://www.abc.com/xx/yy", url);
+        }
+    }
+}
