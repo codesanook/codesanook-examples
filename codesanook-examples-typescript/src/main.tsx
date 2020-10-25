7ea5c40e1273d1fbbed111082d205d4c490d166c
aaronamm <theeranitp@gmail.com>
2020-10-25 17:29:44 +0700

    Set CodeMirror editor to Vim mode

--- codesanook-examples-typescript/src/main.tsx
+++ codesanook-examples-typescript/src/main.tsx
@@ -1,31 +1,34 @@
 import './scss/style.scss';
 import './index.html';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { render } from 'react-dom';
 import { Controlled as CodeMirror } from 'react-codemirror2'
 
 // https://github.com/scniro/react-codemirror2#requiring-codemirror-resources
-import 'codemirror/mode/xml/xml';
-import 'codemirror/mode/javascript/javascript';
+// Ad vim mode
+// https://stackoverflow.com/a/15184747/1872200
+import 'codemirror/addon/dialog/dialog';
+import 'codemirror/addon/search/searchcursor'
+import 'codemirror/mode/clike/clike';
+import 'codemirror/keymap/vim';
 
 const Editor = () => {
-
     const [value, setValue] = useState('');
 
     return (
         <CodeMirror
             options={{
-                mode: 'xml',
                 theme: 'material',
-                lineNumbers: true
+                mode: "text/x-csrc",
+                keyMap: 'vim',
+                lineNumbers: true,
             }}
             value={value}
-            onBeforeChange={(editor, data, editorValue) => {
-                setValue(editorValue);
+            onBeforeChange={(editor, data, value) => {
+                setValue(value);
             }}
-            onChange={(editor, data, editorValue) => {
-                //setValue(editorValue);
-                console.log(editorValue);
+            onChange={(editor, data, value) => {
+                console.log(value);
             }}
         />
     )
