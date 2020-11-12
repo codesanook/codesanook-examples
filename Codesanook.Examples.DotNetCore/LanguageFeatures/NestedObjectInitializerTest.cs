5d55a7ce018c2cfc8bfb955ffcb1aebd3003ca34
aaronamm <theeranitp@gmail.com>
2020-11-12 23:36:18 +0700

    Add property initialize example

---
+++ Codesanook.Examples.DotNetCore/LanguageFeatures/NestedObjectInitializerTest.cs
@@ -0,0 +1,29 @@
+using Xunit;
+
+namespace Codesanook.Examples.DotNetCore.LanguageFeatures
+{
+    public class Person
+    {
+        public Address Address { get; } = new();
+    }
+
+    public class Address
+    {
+        public string Province { get; set; }
+    }
+
+    public class NestedObjectInitializerTest
+    {
+        [Fact]
+        public void NestedPropertyInitialize_PropertyHasInitializedValue_ObjectCreatedAndIsNotNull()
+        {
+            var person = new Person
+            {
+                Address = { Province = "Bangkok" }
+            };
+
+            Assert.NotNull(person);
+        }
+    }
+}
+
