968a60ccbd329f9f2afe835f05ef0845fa6d7782
aaronamm <theeranitp@gmail.com>
2020-12-28 02:10:51 +0700

    Format indention

--- codesanook-examples-typescript/src/components/D3Component.tsx
+++ codesanook-examples-typescript/src/components/D3Component.tsx
@@ -3,45 +3,42 @@ import useInterval from 'use-interval';
 import * as d3 from 'd3';
 
 // Credit https://wattenberger.com/blog/react-and-d3
-
 const Svg = () => {
-    const generateDataset = () => (
-        Array(10).fill(0).map(() => ([
-            Math.random() * 80 + 10,
-            Math.random() * 35 + 10,
-        ]))
-    );
+  const generateDataset = () => (
+    Array(10).fill(0).map(() => ([
+      Math.random() * 80 + 10,
+      Math.random() * 35 + 10,
+    ]))
+  );
 
-    const [dataset, setDataset] = useState(generateDataset());
+  const [dataset, setDataset] = useState(generateDataset());
 
-    const ref = useRef();
-    useEffect(() => {
-        // https://www.d3indepth.com/enterexit/#general-update-pattern
-        const updateSelection = d3.select(ref.current)
-            .selectAll<SVGCircleElement, number[]>('circle')
-            .data(dataset);
+  const ref = useRef();
+  useEffect(() => {
+    // https://www.d3indepth.com/enterexit/#general-update-pattern
+    const updateSelection = d3.select(ref.current)
+      .selectAll<SVGCircleElement, number[]>('circle')
+      .data(dataset);
 
-        const enterSelection = updateSelection.enter();
-        const enterAndUpdateSelection = enterSelection.append('circle').merge(updateSelection);
+    const enterSelection = updateSelection.enter();
+    const enterAndUpdateSelection = enterSelection.append('circle').merge(updateSelection);
+    enterAndUpdateSelection
+      .attr('cx', (d) => d[0])
+      .attr('cy', (d) => d[1])
+      .attr('r', 3);
 
-        enterAndUpdateSelection
-            .attr('cx', d => d[0])
-            .attr('cy', d => d[1])
-            .attr('r', 3);
+    const exitSelection = updateSelection.exit();
+    exitSelection.remove();
+  }, [dataset]);// Render on time when component loaded
 
-        const exitSelection = updateSelection.exit();
-        exitSelection.remove();
+  // https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+  useInterval(() => {
+    const newDataset = generateDataset();
+    setDataset(newDataset);
+    console.log('interval');
+  }, 1000);
 
-    }, [dataset]);// Render on time when component loaded
-
-    // https://overreacted.io/making-setinterval-declarative-with-react-hooks/
-    useInterval(() => {
-        const newDataset = generateDataset();
-        setDataset(newDataset);
-        console.log('interval');
-    }, 1000);
-
-    return <svg viewBox="0 0 100 50" ref={ref} />;
-}
+  return <svg viewBox="0 0 100 50" ref={ref} />;
+};
 
 export default Svg;
--- codesanook-examples-typescript/src/snow/snow.ts
+++ codesanook-examples-typescript/src/snow/snow.ts
@@ -20,72 +20,81 @@ const NUMBER_OF_SNOW_FLAKES = 50;
  * @param Image imageObj The image object to be used as a snow flake
  */
 class SnowFlake {
-    private interval: NodeJS.Timer = null;
-    private x: number;
-    private y: number;
-    private startX: number;
-    private speed: number;
-    private pathWidth: number;
-    private pathHeight: number;
+  private interval: NodeJS.Timer = null;
 
-    constructor(public imageElement: HTMLImageElement) {
-        this.imageElement.style.position = 'absolute';
-        this.imageElement.src = 'snowflake.png';
-        this.reset();
+  private x: number;
+
+  private y: number;
+
+  private startX: number;
+
+  private speed: number;
+
+  private pathWidth: number;
+
+  private pathHeight: number;
+
+  constructor(public imageElement: HTMLImageElement) {
+    this.imageElement.style.position = 'absolute';
+    this.imageElement.src = 'snowflake.png';
+    this.reset();
+  }
+
+  /**
+   * Starts the animation for this object. To stop the animation call stopAnimation.
+   */
+  startAnimation() {
+    this.interval = setInterval(() => this.animation(sinGraph), 100);
+  }
+
+  /**
+   * Stops the animation for this object. To start the animation again call startAnimation.
+   */
+  stopAnimation() {
+    clearInterval(this.interval);
+  }
+
+  // Starts an infinite animation loop using the given function to move
+  // and change the size of the given object.
+  private animation(
+    sinGraph: (yValue: number, adjustedWaveHeight: number, adjustedWaveLength: number) => number,
+  ) {
+    this.y += this.speed; // Keeep increasing y value with small amount or big amount of speed value.
+
+    if (this.pathWidth !== 0 && this.pathHeight !== 0) {
+      this.x = sinGraph(this.y, this.pathWidth, this.pathHeight) + this.startX; // shift on x axis with startX value
     }
 
-    /**
-     * Starts the animation for this object. To stop the animation call stopAnimation.
-     */
-    startAnimation() {
-        this.interval = setInterval(() => this.animation(sinGraph), 100);
+    // Check if snow flake y value is out of the frame
+    if (this.y >= MAX_PATH_HEIGHT) {
+      this.reset();
+    } else {
+      this.imageElement.style.top = `${this.y}px`;
     }
 
-    /**
-     * Stops the animation for this object. To start the animation again call startAnimation.
-     */
-    stopAnimation() {
-        clearInterval(this.interval);
-    };
-
-    // Starts an infinite animation loop using the given function to move and change the size of the given object.
-    private animation(sinGraph: (yValue: number, adjustedWaveHeight: number, adjustedWaveLength: number) => number) {
-        this.y += this.speed; // Keeep increasing y value with small amount or big amount of speed value.
-
-        if (this.pathWidth !== 0 && this.pathHeight !== 0) {
-            this.x = sinGraph(this.y, this.pathWidth, this.pathHeight) + this.startX; // shift on x axis with startX value
-        }
-
-        // Check if snow flake y value is out of the frame
-        if (this.y >= MAX_PATH_HEIGHT) {
-            this.reset();
-        }
-        else {
-            this.imageElement.style.top = `${this.y}px`;
-        }
-
-        if (this.x <= window.innerWidth) {
-            this.imageElement.style.left = `${this.x}px`;
-        }
+    if (this.x <= window.innerWidth) {
+      this.imageElement.style.left = `${this.x}px`;
     }
+  }
 
-    /**
-     * Resets teh status of the object with new random values
-     */
-    private reset() {
-        this.startX = random(0, window.innerWidth);
-        const startY = random(0, MAX_PATH_HEIGHT) * -1;// * -1 To start above browser window, top with negative value. 
-        this.x = this.startX; 
-        this.y = startY;
+  /**
+   * Resets teh status of the object with new random values
+   */
+  private reset() {
+    this.startX = random(0, window.innerWidth);
+    // * -1 To start above browser window, top with negative value.
+    const startY = random(0, MAX_PATH_HEIGHT) * -1;
+    this.x = this.startX;
+    this.y = startY;
 
-        this.speed = random(MIN_SPEED, MAX_SPEED);
-        this.pathWidth = random(MIN_PATH_WIDTH, MAX_PATH_WIDTH);
-        this.pathHeight = random(MIN_PATH_HEIGHT, MAX_PATH_HEIGHT);
+    this.speed = random(MIN_SPEED, MAX_SPEED);
+    this.pathWidth = random(MIN_PATH_WIDTH, MAX_PATH_WIDTH);
+    this.pathHeight = random(MIN_PATH_HEIGHT, MAX_PATH_HEIGHT);
 
-        const size = random(MIN_SIZE, MAX_SIZE);
-        this.imageElement.style.width = `${size}px`;
-        this.imageElement.style.height = `${size}px`;
-    }
+    const size = random(MIN_SIZE, MAX_SIZE);
+    this.imageElement.style.width = `${size}px`;
+    this.imageElement.style.height = `${size}px`;
+  }
 }
 
 /**
@@ -95,26 +104,26 @@ class SnowFlake {
  * @return random number between min (included) and max (not included):
  */
 function random(min: number, max: number) {
-    // https://stackoverflow.com/a/24152886/1872200
-    return Math.random() * (max - min) + min;
+  // https://stackoverflow.com/a/24152886/1872200
+  return Math.random() * (max - min) + min;
 }
 
 // Equation y = h * sin((2PI/w)y)
 // Input y value and return x value
 function sinGraph(yValue: number, adjustedWaveHeight: number, adjustedWaveLength: number) {
-    return adjustedWaveHeight * Math.sin((2 * Math.PI / adjustedWaveLength) * yValue);
+  return adjustedWaveHeight * Math.sin(((2 * Math.PI) / adjustedWaveLength) * yValue);
 }
 
 export default function initAnimation() {
-    // Create snow flakes
-    const snowFlakes = [...Array(NUMBER_OF_SNOW_FLAKES).keys()].map(() => {
-        const imageElement = document.createElement("img") as HTMLImageElement;
-        return new SnowFlake(imageElement);
-    });
+  // Create snow flakes
+  const snowFlakes = [...Array(NUMBER_OF_SNOW_FLAKES).keys()].map(() => {
+    const imageElement = document.createElement('img') as HTMLImageElement;
+    return new SnowFlake(imageElement);
+  });
 
-    const bodyElement = document.getElementsByTagName('body')[0];
-    snowFlakes.forEach(snowFlake => {
-        bodyElement.appendChild(snowFlake.imageElement);
-        snowFlake.startAnimation();
-    });
+  const bodyElement = document.getElementsByTagName('body')[0];
+  snowFlakes.forEach((snowFlake) => {
+    bodyElement.appendChild(snowFlake.imageElement);
+    snowFlake.startAnimation();
+  });
 }
