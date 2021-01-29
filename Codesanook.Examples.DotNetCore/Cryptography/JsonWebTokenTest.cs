a1bcf4a4cec16f13789cb0c81ed23ce83a58f6f1
aaronamm <theeranitp@gmail.com>
2021-01-29 23:16:39 +0700

    More examples in JWT .NET

--- Codesanook.Examples.DotNetCore/Cryptography/JsonWebTokenTest.cs
+++ Codesanook.Examples.DotNetCore/Cryptography/JsonWebTokenTest.cs
@@ -1,17 +1,18 @@
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.JsonWebTokens;
 using System;
-using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using Xunit;
 using Xunit.Abstractions;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace Codesanook.Examples.DotNetCore.Cryptography
 {
     public class JsonWebTokenTest
     {
         private readonly ITestOutputHelper output;
-
         public JsonWebTokenTest(ITestOutputHelper output) => this.output = output;
 
         [Fact]
@@ -19,7 +20,7 @@ namespace Codesanook.Examples.DotNetCore.Cryptography
         {
             // Arange
             // generate token that is valid for 7 days
-            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenHandler = new JsonWebTokenHandler();
             var secretKey = Encoding.ASCII.GetBytes("MysecretMysecretMysecret");
             var securityKey = new SymmetricSecurityKey(secretKey);
             var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
@@ -31,9 +32,7 @@ namespace Codesanook.Examples.DotNetCore.Cryptography
                 SigningCredentials = signingCredentials
             };
             var token = tokenHandler.CreateToken(tokenDescriptor);
-            var tokenString = tokenHandler.WriteToken(token);
-
-            output.WriteLine($"token: {tokenString}");
+            output.WriteLine($"token: {token}");
 
             // output on jwt.io
             /*
@@ -60,14 +59,55 @@ namespace Codesanook.Examples.DotNetCore.Cryptography
                 ValidAudience = null,
             };
 
-            var exception = Record.Exception(() => 
-            {
-               tokenHandler.ValidateToken(tokenString, tokenValidationParameters, out SecurityToken validatedToken);
-            });
+            var validationResult = tokenHandler.ValidateToken(token, tokenValidationParameters);
+            // var exception = Record.Exception(() => { });
+            // Assert.IsNotType<SecurityTokenInvalidSignatureException>(exception);
 
             // Assert
-            // Assert.IsNotType<SecurityTokenInvalidSignatureException>(exception);
-            Assert.Null(exception);
+            Assert.True(validationResult.IsValid);
+        }
+
+        // https://github.com/AzureAD/azure-activedirectory-identitymodel-extensions-for-dotnet/blob/dev/src/Microsoft.IdentityModel.Tokens/SymmetricSignatureProvider.cs#L188
+        [Fact]
+        public void CreateTokenWithKeyHashDirectly_ValidInput_ReturnValidToken()
+        {
+            // Arange
+            // generate token that is valid for 7 days
+            var secretKey = Encoding.ASCII.GetBytes("MysecretMysecretMysecret");
+            var securityKey = new SymmetricSecurityKey(secretKey);
+            // var cryptoProviderFactory = securityKey.CryptoProviderFactory;
+            var cryptoProviderFactory = new CryptoProviderFactory();
+            var keyBytes = securityKey.Key;
+            var keyedHash = cryptoProviderFactory.CreateKeyedHashAlgorithm(keyBytes, SecurityAlgorithms.HmacSha256);
+
+            var header = new JObject()
+            {
+                { JwtHeaderParameterNames.Alg, SecurityAlgorithms.HmacSha256 }, // HS256
+                { "typ", "JWT" }
+            };
+
+            var payload = new JObject()
+            {
+                { "id", "1" }
+            };
+
+            var rawHeader = Base64UrlEncoder.Encode(Encoding.UTF8.GetBytes(header.ToString(Formatting.None)));
+            var rawPayloader = Base64UrlEncoder.Encode(Encoding.UTF8.GetBytes(payload.ToString(Formatting.None)));
+
+            var inputForSignature = $"{rawHeader}.{rawPayloader}";
+            var signature = Base64UrlEncoder.Encode(keyedHash.ComputeHash(Encoding.UTF8.GetBytes(inputForSignature)));
+
+            var token = $"{rawHeader}.{rawPayloader}.{signature}";
+            output.WriteLine($"token: {token}");
+            /*
+            {
+              "alg": "HS256",
+              "typ": "JWT"
+            }
+            {
+              "id": "1"
+            }
+            */
         }
     }
 }
