6b84d464e3757996a47eb2f0e9b1af6a0b06ecaf
aaronamm <theeranitp@gmail.com>
2021-04-02 15:11:30 +0700

    Add headless and headful tests

--- codesanook-examples-playwright/src/__tests__/homepage.test.ts
+++ codesanook-examples-playwright/src/__tests__/homepage.test.ts
@@ -1,11 +1,27 @@
-import { chromium } from 'playwright';
+import { webkit } from 'playwright';
+jest.setTimeout(10000);
 
 // Uppercase name
 describe('Homepage', () => {
 
   // Lowercase name
-  test('should launch homepage', async () => {
-    const browser = await chromium.launch();
+  test('should launch homepage with headless mode', async () => {
+    const browser = await webkit.launch();
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    // Log and continue all network requests
+    page.route('**', route => {
+      console.log(route.request().url());
+      route.continue();
+    });
+
+    await page.goto('http://todomvc.com');
+    await browser.close();
+  });
+
+  test('should launch homepage with headful mode', async () => {
+    const browser = await webkit.launch({ headless: false });
     const context = await browser.newContext();
     const page = await context.newPage();
 
