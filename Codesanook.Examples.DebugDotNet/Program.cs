9a9654fb37bb51d96cccb9c0cb507af1817bbaea
aaronamm <theeranitp@gmail.com>
2021-04-19 21:30:11 +0700

    Fix project structure

---
+++ Codesanook.Examples.DebugDotNet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Codesanook.Examples.DebugDotNet
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var values = new[] { "a", "b", "c" };
+            var output = values.Select((v, i) => $"index {i}: value: {v}");
+            Console.WriteLine(string.Join("\n", output));
+        }
+    }
+}
